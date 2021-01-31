e739a25a8775c48f75ff002db8e9117709688f41
CR <931009686@qq.com>
2021-01-31 21:32:15 +0800

    商品服务-API-品牌管理-JSR303自定义校验注解

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -209,7 +209,7 @@
         const {brandId, showStatus} = data
         // 发送请求修改状态
         this.$http({
-          url: this.$http.adornUrl('/product/brand/update'),
+          url: this.$http.adornUrl('/product/brand/update/status'),
           method: 'post',
           data: this.$http.adornData({brandId, showStatus}, false)
         }).then(() => {
